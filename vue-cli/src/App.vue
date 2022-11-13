1e091e8613a3f5d7bd7d2daa7829dac3a9756042
[전시개발팀]이창근 <changgeun.lee@lotte.net>
2022-11-14 07:58:15 +0900

    feat: vue cli

---
+++ vue-cli/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <app-header v-bind:propsdata="str" v-on:renew="renewStr"></app-header>
+  </div>
+</template>
+
+<script>
+import AppHeader from "@/components/AppHeader";
+
+export default {
+  data: function () {
+    return {
+      str: 'hi'
+    }
+  },
+  components: {
+    'app-header': AppHeader
+  },
+  methods: {
+    renewStr: function () {
+      this.str = 'hello';
+    }
+  }
+};
+</script>
+
+<style>
+</style>
